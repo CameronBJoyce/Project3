c31080a445081283a8e31b158f3c577eabed8380
Cameron Joyce <61301130+CameronBJoyce@users.noreply.github.com>
2021-03-27 08:32:42 -0400

    Update besttrade.c

--- besttrade.c
+++ besttrade.c
@@ -1,6 +1,5 @@
 
-// Starter code provided by David Sykes. All four functions have stubs
-// Code completed by the ethereal Bennett Joyce and the polymath Ivan Gu
+// Code completed by Bennett Joyce and the polymath Ivan Gu
 //
 
 #include <stdio.h>
